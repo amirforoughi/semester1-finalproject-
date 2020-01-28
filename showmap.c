ae3bd796ecf19a7471dc9e6ec048f42f010c000c
amir foroughi <mahsa.foroughi36@yahoo.com>
2020-01-28 13:46:26 +0330

    minor fixes

--- showmap.c
+++ showmap.c
@@ -1,10 +1,59 @@
+/*
 #include<stdio.h>
-int main(){
-    int i = 0 , j = 0 , n = 5 ,k=n=11;
+#include<stdlib.h>
+#include<conio.h>
+#include<windows.h>
+#include<time.h>
+#include<math.h>
+#include<string.h>
+#include <dos.h>
+#include <dir.h>
+*/
+#include"myheaders.h"
+void SetColor(int ForgC)
+ {
+ WORD wColor;
+
+  HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+  CONSOLE_SCREEN_BUFFER_INFO csbi;
+
+                       //We use csbi for the wAttributes word.
+ if(GetConsoleScreenBufferInfo(hStdOut, &csbi))
+ {
+                 //Mask out all but the background attribute, and add in the forgournd color
+      wColor = (csbi.wAttributes & 0xF0) + (ForgC & 0x0F);
+      SetConsoleTextAttribute(hStdOut, wColor);
+ }
+ return;
+}
+
+/*struct mapEl** creat_map(int n){
+    struct mapEl** map=(struct mapEl**)malloc( sizeof(mapEl*) * n );
+    for(int i = 0 ; i < n ; i++){
+        map[i]=(struct mapEl*)malloc( sizeof(mapEl) * n);
+    }
+    return map;
+}*/
+
+void showMap(int n2 , struct cell * head , struct mapEl** map){
+    char c;
+    int n =  int n = 3 * n2 +2;;
+    /*
+    if( n2 == 3)
+        n = 11;
+    if( n2 == 4)
+        n = 14;
+    if( n2 == 2)
+        n = 8;
+    if( n2 == 8)
+        n = 26;
+    */
+    int i = 0 , j = 0 , k = n ;
+    int m= (n-2)/6 ;
 
         for( i=0 ; i<n ; i++ ){
             if ( i == 0){
-                    for(j=3;j>0;j--){
+                    for(j=0;j<m;j++){
                         printf(" _______        ");
                     }
 
@@ -12,7 +61,7 @@ int main(){
                         continue;
             }
             if( i == 1){
-                for(j=3;j>0;j--){
+                for(j=0;j<m;j++){
                         printf("I       I_______");
                      }
                      // printf("I");
@@ -22,7 +71,7 @@ int main(){
 
             if (i == n-1){
                 printf("        I_______");
-                     for(j=2;j>0;j--){
+                     for(j=0;j<m-1;j++){
                         printf("I       I_______");
                      }
                       printf("I");
@@ -30,36 +79,54 @@ int main(){
                         continue;
             }
             if (i%3 == 1){
-                     for(j=3;j>0;j--){
+                     for(j=0;j<m;j++){
                         printf("I       I_______");
                      }
                       printf("I");
                         printf("\n");
             }
             if (i%3 == 2){
-                     for(j=3;j>0;j--){
-                        printf("I  P2  0I       ");
+                     for(j=0;j<m;j++){
+                        c=check_block(head , (n-2)/3 , (i+1)/3 , 2*j+1);
+                        printf("I  %c   0I       ",c);
+
                      }
                       printf("I");
                         printf("\n");
             }
             if (i%3 == 0){
-                     for(j=3;j>0;j--){
-                        printf("I_______I  P2  0");
+                     for(j=0;j<m;j++){
+                        c=check_block(head ,  (n-2)/3  , (i+2)/3 , 2*j+2);
+                        printf("I_______I  %c   0",c);
                      }
                       printf("I");
                         printf("\n");
             }
         }
-
+}
     /*   _________
-        I         I_________I
+        I         I_________
         I         I Player1 I
         I_________I double  I
         I  keep   I_________I
-        I  out!!  I Player3 I
+        I  out!!  I Player3 I int n = 3 * np +2;
         I_________I  E=100  I
                   I_________I
-        */
+*/
+// column does not have a zerro
+char check_block( struct cell * head , int n , int column , int row){
+    struct cell * current = head ;
 
+    //struct cell * current2 = head ;
+    static int j = 0;
+    int yp , xp = row-1;
+    for( yp = n-column , xp = row-1 ; current != NULL  ; current = current->next ){
+        if( current->x == xp && current->y == yp ){
+            return 'X';
+        }
+
+    }
+    return ' ';
 }
+
+
